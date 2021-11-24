1205b76d86df015507b43584fa5c8b65ecb1b518
Troy <troyd9216@gmail.com>
2021-11-24 17:04:25 -0600

    updated

--- buildspace-nft-game-starter/src/App.js
+++ buildspace-nft-game-starter/src/App.js
@@ -1,23 +1,115 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
+import SelectCharacter from './Components/SelectCharacter';
 // Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const [currentAccount, setCurrentAccount] = useState(null);
+  const [characterNFT, setCharacterNFT] = useState(null)
+  /*
+  * Just a state variable we use to store our user's public wallet. Don't forget to import useState.
+  */
+  /*
+   * Since this method will take some time, make sure to declare it as async
+   */
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log('Make sure you have MetaMask!');
+        return;
+      } else {
+        console.log('We have the ethereum object', ethereum);
+
+        /*
+         * Check if we're authorized to access the user's wallet
+         */
+        const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+        /*
+         * User can have multiple authorized accounts, we grab the first one if its there!
+         */
+        if (accounts.length !== 0) {
+          const account = accounts[0];
+          console.log('Found an authorized account:', account);
+          setCurrentAccount(account);
+        } else {
+          console.log('No authorized account found');
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window
+
+      if (!ethereum) {
+        alert("Please install MetaMask");
+        return
+      }
+
+      //method to request access to account
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      })
+
+      console.log("connected", accounts[0])
+      setCurrentAccount(accounts[0])
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const renderContent = () => {
+    if (!currentAccount) {
+      return (
+        <div className="connect-wallet-container">
+          <img
+            src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
+            alt="Monty Python Gif"
+          />
+          <button
+            className="cta-button connect-wallet-button"
+            onClick={connectWallet}
+          >
+            Connect Wallet To Get Started
+          </button>
+        </div>
+      );
+      /*
+       * Scenario #2
+       */
+    } else if (currentAccount && !characterNFT) {
+      return <SelectCharacter setCharacterNFT={setCharacterNFT} />;
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+  // this returns our function when the page loads
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
           <p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
+          {renderContent()}
           <p className="sub-text">Team up to protect the Metaverse!</p>
           <div className="connect-wallet-container">
-            <img
-              src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
-              alt="Monty Python Gif"
-            />
+            <button onClick={connectWallet} className="cta-button connect-wallet-button">Connect to your wallet!</button>
           </div>
         </div>
         <div className="footer-container">
